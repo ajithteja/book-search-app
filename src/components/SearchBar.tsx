e5b421943f09b9429ab9bf7690918cddfaaafb85
Ajith Teja <ajithtejag@gmail.com>
2025-04-10 18:17:33 +0530

    removed unused data

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 export default function SearchBar({
@@ -11,7 +11,7 @@ export default function SearchBar({
   const [query, setQuery] = useState(initialQuery);
   // const [isSearching, setIsSearching] = useState(false);
   const router = useRouter();
-  const searchParams = useSearchParams();
+  // const searchParams = useSearchParams();
 
   // Reset isSearching when query param changes (after search completes)
   // useEffect(() => {
